2bb7ec5a664a8c86689cd128616886ecad231555
satish7777777 <99494605+satish7777777@users.noreply.github.com>
2023-05-23 10:15:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-import express from "express";
+import http from "http";
+//const vik = require('./features');
+//const toi = require('./features');
+import love from './features.js';
+import { sat,twi } from "./features.js";
 
-const app = express();
+console.log(sat);
+console.log(twi);
+console.log(love);
 
-app.listen(8500, ()=>{
-    console.log("hello world");
+const server = http.createServer((req, res)=>{
+    if(req.url==="/about"){
+        res.end("<h1>About</h1>");
+    }
+    else if(req.url==="/contact"){
+        res.end("<h1>Contact</h1>");
+    }
+    else if(req.url==="/"){
+        res.end("<h1>Home</h1>");
+    }
+    else{
+        res.end("<h1>Page not found</h1>");
+    }   
 });
+
+server.listen(8000, ()=>{
+    console.log("Server is created");
+    console.log("Server is started");
+   
+})
